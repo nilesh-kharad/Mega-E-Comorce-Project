8d78827bbb13cb026f56fb7c440772d9cfacc83d
Nilesh kharad <nileshk191@gmail.com>
2021-07-02 16:47:41 +0530

    header,footer,login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="card w-100">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">E-Commerce</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+          
+           
+        </ul>
+    </div>
+</nav>
